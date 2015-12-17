8c7776cd5163a0ccf5387c4eaf4bc059ae5c0774
daria <zychowicz.daria@gmail.com>
2015-12-17 16:41:20 +0100

    commit 17 Dec 2014 16:41

--- js/app.js
+++ js/app.js
@@ -1,7 +1,72 @@
 /**
  * Created by daria on 10.12.15.
  */
-$(document).ready(function() {
-console.log("dzoala");
+//scroll
+$(document).ready(function(){
+
+    function scrollPage() {
+        var menuItems = $(".menuScroll").children();
+        menuItems.on("click", function(event){
+            event.preventDefault();
+            console.log(this);
+            var href = $(this).find("a").attr("href");
+            console.log(href);
+            $('html, body').animate({
+                scrollTop: $(href).offset().top
+            }, 2000);
+        });
+     }
+    scrollPage();
+//sticky menu
+
+    var menu = $(".menu");
+    var menuOffsetFromTop = menu.position().top;
+
+    $(window).on("scroll", function(event){
+        //console.log(window).scrollTop());
+        if($(window).scrollTop() > menuOffsetFromTop) {
+            menu.addClass("sticky");
+        }
+        else{
+            menu.removeClass("sticky");
+        }
+
+    });
 
 });
+
+
+//document.addEventListener('DOMContentLoaded', function () {
+//
+//    var listWithPictures = document.querySelectorAll("flex-slider li");
+//    var prevButton = document.getElementById("arrowleftButton");
+//    var nextButton = document.getElementById("arrowrightButton");
+//    var biggerPicture = 0;
+//
+//    listWithPictures[biggerPicture].classList.add("smaller");
+//
+//    prevButton.addEventListener("click", function(event) {
+//        listWithPictures[biggerPicture].classList.remove("smaller");
+//        biggerPicture--;
+//        if(biggerPicture < 0)  {
+//            biggerPicture = listWithPictures.length-1;
+//        }
+//        listWithPictures[biggerPicture].classList.remove("smaller");
+//    });
+//
+//    nextButton.addEventListener("click", function(event) {
+//        listWithPictures[biggerPicture].classList.remove("smaller");
+//        biggerPicture++;
+//        if(biggerPicture >= listWithPictures.length){
+//            biggerPicture = 0;
+//        }
+//        listWithPictures[biggerPicture].classList.remove("smaller");
+//    });
+//
+//
+//});
+
+
+
+//formulate validation
+
